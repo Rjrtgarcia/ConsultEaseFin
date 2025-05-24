9e30083e4e0dd3a2212eb51fd63bf4cf72c54d0c
Rjrtgarcia <rjrtgarcia@gmail.com>
2025-05-24 08:19:09 +0800

    9

--- central_system/views/__init__.py
+++ central_system/views/__init__.py
@@ -1,6 +1,7 @@
 from .base_window import BaseWindow
 from .login_window import LoginWindow
-from .dashboard_window import DashboardWindow, FacultyCard, ConsultationRequestForm
+from .dashboard_window import DashboardWindow, FacultyCard
+from .consultation_panel import ConsultationRequestForm
 from .admin_login_window import AdminLoginWindow
 from .admin_dashboard_window import AdminDashboardWindow, FacultyManagementTab, StudentManagementTab, SystemMaintenanceTab
 
